79e5d7c83a8b00deec84a7da3335158054fda26a
Kai Shao <grepug@icloud.com>
2024-07-19 17:55:06 +0800

    add DifyServerPostContent

--- Sources/ContextSharedModels/Prompt/DifyServerPostContent.swift
+++ Sources/ContextSharedModels/Prompt/DifyServerPostContent.swift
@@ -1,18 +1,16 @@
 //
-//  DifyPostContent.swift
+//  DifyServerPostContent.swift
 //
 //
 //  Created by Kai Shao on 2024/7/18.
 //
 
-public struct DifyPostContent<I: CoSendable>: CoSendable {
-    public let id: String
+public struct DifyServerPostContent<I: CoSendable>: CoSendable {
     public let user: String
     public let inputs: I
     public let response_mode: String
 
-    public init(id: String, user: String, inputs: I, stream: Bool) {
-        self.id = id
+    public init(user: String, inputs: I, stream: Bool) {
         self.user = user
         self.inputs = inputs
         self.response_mode = stream ? "streaming" : "block"
