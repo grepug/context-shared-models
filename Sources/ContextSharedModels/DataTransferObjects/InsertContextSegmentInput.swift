3dc4d3a2f1f0b14ba4bcd856d1a691155c90bd9d
Kai Shao <grepug@icloud.com>
2024-07-23 10:46:19 +0800

    update

--- Sources/ContextSharedModels/DataTransferObjects/InsertContextSegmentInput.swift
+++ Sources/ContextSharedModels/DataTransferObjects/InsertContextSegmentInput.swift
@@ -1,6 +1,11 @@
 import Foundation
 
-struct InsertContextSegmentInput: CoSendable {
-    let contextId: String
-    let segment: ContextModel.ContextSegment
+public struct InsertContextSegmentInput: CoSendable {
+    public let contextId: String
+    public let segment: ContextModel.ContextSegment
+
+    public init(contextId: String, segment: ContextModel.ContextSegment) {
+        self.contextId = contextId
+        self.segment = segment
+    }
 }
