7e7e410a9e82aac379c9834d1bfb4b2440344732
Kai Shao <grepug@icloud.com>
2024-08-07 12:13:12 +0800

    add print to codable utils

--- Sources/ContextSharedModels/Utils/CodableUtils.swift
+++ Sources/ContextSharedModels/Utils/CodableUtils.swift
@@ -25,19 +25,21 @@ extension Encodable {
 }
 
 extension Decodable {
-    public static func fromData(_ data: Data) throws -> Self {
+    public static func fromData(_ data: Data, print shouldPrint: Bool = true) throws -> Self {
         do {
             return try JSONDecoder().decode(Self.self, from: data)
         } catch {
-            print("decoding error: \(error)", "string: \(String(data: data, encoding: .utf8) ?? "no string")")
+            if shouldPrint {
+                print("decoding error: \(error)", "string: \(String(data: data, encoding: .utf8) ?? "no string")")
+            }
 
             throw error
         }
     }
 
-    public static func fromString(_ string: String) throws -> Self {
+    public static func fromString(_ string: String, print: Bool = true) throws -> Self {
         do {
-            return try Self.fromData(Data(string.utf8))
+            return try Self.fromData(Data(string.utf8), print: print)
         } catch {
             throw error
         }
