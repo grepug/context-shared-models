365bf6dbda801e1839e9cd95e0b3abec98cb659d
Kai Shao <grepug@icloud.com>
2024-08-22 17:15:26 +0800

    change FindSameTextSegmentsInput

--- Sources/ContextSharedModels/DataTransferObjects/FindSameTextSegmentsInput.swift
+++ Sources/ContextSharedModels/DataTransferObjects/FindSameTextSegmentsInput.swift
@@ -1,9 +1,11 @@
 import Foundation
 
 public struct FindSameTextSegmentsInput: CoSendable {
-    public let segmentId: String
+    public let segmentId: String?
+    public let text: String?
 
-    public init(segmentId: String) {
+    public init(segmentId: String? = nil, text: String? = nil) {
         self.segmentId = segmentId
+        self.text = text
     }
 }
