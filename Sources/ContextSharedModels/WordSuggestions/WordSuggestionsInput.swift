8cf5381c851b224db5a07aeac5ebeb7c561e2de6
Kai Shao <grepug@icloud.com>
2024-08-26 11:47:32 +0800

    changed TextRangeLookUpInput

--- Sources/ContextSharedModels/WordSuggestions/WordSuggestionsInput.swift
+++ Sources/ContextSharedModels/WordSuggestions/WordSuggestionsInput.swift
@@ -16,9 +16,13 @@ public struct WordSuggestionInput: CoSendable {
 public struct TextRangeLookUpInput: CoSendable {
     public let text: String
     public let range: [Int]
+    public let tokenText: String
+    public let ajacentText: String
 
-    public init(text: String, range: [Int]) {
+    init(text: String, range: [Int], tokenText: String, ajacentText: String) {
         self.text = text
         self.range = range
+        self.tokenText = tokenText
+        self.ajacentText = ajacentText
     }
 }
