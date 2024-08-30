e951e124caec34a087dec22a4d803a917d27a708
Kai Shao <grepug@icloud.com>
2024-08-30 10:51:42 +0800

    fix

--- Sources/ContextSharedModels/DataTransferObjects/FindSameExistingTokenRangesInput.swift
+++ Sources/ContextSharedModels/DataTransferObjects/FindSameExistingTokenRangesInput.swift
@@ -1,6 +1,6 @@
 public struct FindSameExistingTokenRangesInput: CoSendable {
-    let text: String
-    let contextId: String
+    public let text: String
+    public let contextId: String
 
     public init(text: String, contextId: String) {
         self.text = text
