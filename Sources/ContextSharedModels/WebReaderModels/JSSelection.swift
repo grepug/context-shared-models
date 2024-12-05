0cf334f092e6a0e7d4be2295ad1cc012ae6c0726
Kai Shao <grepug@icloud.com>
2024-12-05 16:44:24 +0800

    refactor JSSelection to use combined string as ID instead of SHA256 hash

--- Sources/ContextSharedModels/WebReaderModels/JSSelection.swift
+++ Sources/ContextSharedModels/WebReaderModels/JSSelection.swift
@@ -6,24 +6,20 @@
 //
 
 import Foundation
-import CryptoKit
 
 public struct JSSelection: CoSendable, Hashable {
     public let string: String
     public let serializedRange: JSRange
     public let bounds: JSDOMRect
     public let id: String
-    
+
     public init(string: String, range: JSRange, bounds: JSDOMRect) {
         self.string = string
         self.serializedRange = range
         self.bounds = bounds
-        
+
         // Convert the properties into a single string
         let combinedString = string + (try! serializedRange.toString()) + (try! bounds.toString())
-        
-        // Compute the SHA256 hash for the combined string
-        let digest = SHA256.hash(data: combinedString.data(using: .utf8)!)
-        self.id = digest.compactMap { String(format: "%02x", $0) }.joined()
+        self.id = combinedString
     }
 }
