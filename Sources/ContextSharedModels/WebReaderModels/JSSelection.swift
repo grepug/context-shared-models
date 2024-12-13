ab45529b5332270e91ca00ba13b3368930abb7a1
Kai Shao <grepug@icloud.com>
2024-12-13 13:40:42 +0800

    added comments

--- Sources/ContextSharedModels/WebReaderModels/JSSelection.swift
+++ Sources/ContextSharedModels/WebReaderModels/JSSelection.swift
@@ -10,11 +10,13 @@ import Foundation
 public struct JSSelection: CoSendable, Hashable {
     public let id: String
     public let string: String
-    public let rangeItem: JSRange
+    public let rangeItem: JSRange?
+    public let offset: Int?
     
-    public init(id: String, string: String, range: JSRange) {
+    public init(id: String, string: String, range: JSRange? = nil, offset: Int? = nil) {
         self.id = id
         self.string = string
         self.rangeItem = range
+        self.offset = offset
     }
 }
