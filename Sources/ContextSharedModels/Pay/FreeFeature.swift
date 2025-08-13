96cdce14de2fdee49f84d964a8420935a31040c4
Kai Shao <grepug@icloud.com>
2025-08-13 10:35:39 +0800

    add FreeFeature enum and FreeFeatureLimitInfo struct for feature management

---
+++ Sources/ContextSharedModels/Pay/FreeFeature.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+public enum FreeFeature: String, CaseIterable {
+    case importFulltext
+    case addContextSegment
+    case contextTranslation
+    case contextStudyNote
+    case segmentStudyNote
+
+    public var countLimit: Int {
+        switch self {
+        case .importFulltext: 5
+        case .addContextSegment: 100
+        case .contextTranslation: 3
+        case .contextStudyNote: 3
+        case .segmentStudyNote: 10
+        }
+    }
+
+    public var localizedName: String {
+        switch self {
+        case .importFulltext: "导入文章"
+        case .addContextSegment: "添加生词"
+        case .contextTranslation: "翻译"
+        case .contextStudyNote: "语法"
+        case .segmentStudyNote: "单词学习"
+        }
+    }
+}
+
+public struct FreeFeatureLimitInfo {
+    public var featureCanUse: [FreeFeature: Bool] = [:]
+    public var featureNextAvailableDate: [FreeFeature: Date?] = [:]
+}
